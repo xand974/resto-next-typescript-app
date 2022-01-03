8acc4522c3c3184c7c9cf7a0aebdd68325e3a41b
xand974 <xand974@gmail.com>
2022-01-03 22:43:22 +0100

    api : setup

---
+++ api/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+dotenv.config();
+const app = express();
+const PORT = 5000 || process.env.PORT;
+
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(helmet());
+app.use(morgan("common"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => {
+  console.log(`port is connected : ${PORT}`);
+});
